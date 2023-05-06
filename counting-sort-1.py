a9f9bb1bb79537c9b98c453d86bed72d60446f6c
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-05-07 01:32:35 +0530

    Create counting-sort-1.py

---
+++ counting-sort-1.py
@@ -0,0 +1,41 @@
+# Hackerrank
+
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-countingsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    result = []
+    for i in range(len(arr)):
+        if i >= 0 and i < 100:
+            result.append(arr.count(i))
+        
+    return result                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
