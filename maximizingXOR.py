a6dc223c7ab02aa4e6b877ca9599912bace4c00a
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-07-16 16:02:10 +0530

    Create maximizingXOR.py

---
+++ maximizingXOR.py
@@ -0,0 +1,43 @@
+# Hackerrank
+# https://www.hackerrank.com/challenges/maximizing-xor/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximizingXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER l
+#  2. INTEGER r
+#
+
+def maximizingXor(l, r):
+    # Write your code here
+    max = 0
+    for i in range(l,r+1):
+        for j in range(i,r+1):
+            val = i ^ j
+            print(i,j)
+            if val > max:
+                max = val
+    return max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    r = int(input().strip())
+
+    result = maximizingXor(l, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
