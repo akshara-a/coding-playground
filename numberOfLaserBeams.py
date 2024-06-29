b50024c38eee0cf29b6c50c948692a89fd53a4f1
Akshara A <68069921+akshara-a@users.noreply.github.com>
2024-06-29 19:20:53 +0530

    Update numberOfLaserBeams.py

--- numberOfLaserBeams.py
+++ numberOfLaserBeams.py
@@ -8,3 +8,13 @@ class Solution:
             count.append(i.count('1'))
 
         temp = [0] * len(count)
+        for i in temp: 
+            if i in count: 
+                count.remove(i)
+                
+        output = 0 
+
+        for i in range(len(count)-1):
+            output += (count[i] * count[i+1])
+
+        return output
